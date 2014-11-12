bc615faa47bf741c56fd7c9e433d038ee7d279fd
hbc <bcxxxxxx@gmail.com>
2014-11-12 10:32:19 +0800

    Import codebase.

---
+++ src/shire/bcho/palantiroid/palantir/Model/Ticket.java
@@ -0,0 +1,9 @@
+package shire.bcho.palantiroid.palantir.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Ticket {
+
+    @SerializedName("ticket")
+    public String ticket;
+}

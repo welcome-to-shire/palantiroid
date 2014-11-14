7e79d3a0fc5aeff7a46c2997d311c815eabcc892
hbc <bcxxxxxx@gmail.com>
2014-11-14 22:15:21 +0800

    Revert "Receive boot complete signal only."
    This reverts commit c0c4b87bf121ef333162e524c728879774632281.

--- src/shire/bcho/palantiroid/PalantirActivity.java
+++ src/shire/bcho/palantiroid/PalantirActivity.java
@@ -11,10 +11,18 @@ import android.content.Intent;
  */
 public class PalantirActivity extends Activity
 {
+    private final static String BOOT_INTENT = "shire.bcho.palantiroid.BOOT_COMPLETED";
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Start notification receiver
+        // FIXME receiver should start via `BOOT_COMPLETED`
+        Intent intent = new Intent();
+        intent.setAction(BOOT_INTENT);
+        sendBroadcast(intent);
     }
 }
